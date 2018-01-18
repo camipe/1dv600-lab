8202657336fdd48d97453f3299660b6a741caa5a
Micael Persson <micaelpersson@Micaels-MacBook-Air.local>
2018-01-18 10:36:36 +0100

    Completed Task 1 - Subtask A.

---
+++ app/dao/Book.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(id, title, author, genre, publishDate, price, description) {
+  this.id = id,
+  this.title = title,
+  this.author = author,
+  this.genre = genre,
+  this.publishDate = publishDate,
+  this.price = price,
+  this.description = description
+  }
+}
+
+module.exports = Book;
--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -2,10 +2,18 @@
     "use strict";
 
     var LibraryDAO = require('../dao/LibraryDAO');
+    var Book = require('../dao/Book');
 
 
     module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-
+        const bible = new Book(1, 'Bible', 'God', 'Religous', '1337-01-02', 50, "A book to live by");
+        const foundation = new Book(2, 'Foundation', 'Isaac Asimov', 'Science Fiction', '1951-08-21', 164, 'About robots and stuff');
+        const hp = new Book(3, 'Harry Potter', 'JK Rowling', 'Fantasy', '2001-09-11', 100, 'The story about a magic boy');
+        
+        const books = [bible, foundation, hp];
+        
+        console.log(books);
+        return bible;
     };
 
 }());
